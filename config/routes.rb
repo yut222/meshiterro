2faa0ac4084e137f4de39c2067721e83a5a8420b
EC2 Default User <ec2-user@ip-172-31-6-238.ap-northeast-1.compute.internal>
2023-01-03 16:44:49 +0000

    投稿機能の作成(2) - モデルを準備しよう(2) 9章

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-
+  root to: 'homes#top'
   devise_for :users
-  root to: "homes#top"
-  
-  get 'postimages/new'
-  get 'postimages/index'
-  get 'postimages/show'
 
-  get 'homes/about' => 'homes#about', as: 'about'
+  resources :post_images, only: [:new, :index, :show]
+
+  get "/homes/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
