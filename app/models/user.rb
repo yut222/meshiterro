7c194632fc4327a1658add053176ebedc3a2f9eb
EC2 Default User <ec2-user@ip-172-31-6-238.ap-northeast-1.compute.internal>
2023-01-07 08:20:35 +0000

    [Finish] ユーザーに関する機能(1) - 準備編 14章

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,12 @@ class User < ApplicationRecord
   # ユーザーごとのプロフ画像を(保存)扱うため追記
   has_one_attached :profile_image  #profile_image という名前でActiveStorageでプロフ画像を保存できる設定
   
-  def get_profile_image(width, height)  #PostImageモデルの中に記述することで、カラムを呼び出すようにこの処理（メソッド）を呼び出すことができる
-    unless profile_image.attached?
-    file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+  def get_profile_image(width, height)  #Userモデルの中に記述することで、カラムを呼び出すようにこの処理（メソッド）を呼び出すことができる.
+    unless profile_image.attached?  # .attached? imageが存在するかを確認するメソッド
+    file_path = Rails.root.join('app/assets/images/sample-author1.jpg')  #profile_imageがなければsample-author1.jpgが表示される
     profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    profile_image.variant(resize_to_limit: [width, height]).processed  #画像サイズの変更(メソッドに対して引数に設定した値に画像のサイズを変換する)
+    profile_image.variant(resize_to_limit: [width, height]).processed  #画像サイズの変更(メソッドに対して引数に設定した値に画像のサイズを変換する),
   end
   
 end
