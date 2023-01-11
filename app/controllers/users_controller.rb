ebabf0cc9aa6b81245e9b992f894def86baeeaad
EC2 Default User <ec2-user@ip-172-31-6-238.ap-northeast-1.compute.internal>
2023-01-11 17:27:16 +0000

    [Start] deviseの設定変更を学ぼう

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  # ここから追加
+  before_action :is_matching_login_user, only: [:edit, :update]
+  # ここまで追加
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])
@@ -7,17 +10,25 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path(current_user)
+    redirect_to user_path(@user.id)
   end
-  
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
-  
+
+  # ここから追加
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    unless user_id == current_user.id
+      redirect_to post_images_path
+    end
+  end
+  # ここまで追加
 end
